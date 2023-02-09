1b5d9f8b868e63eedd51a60dc09edb9ae90f6953
nickguerra <nickg@maliciousgames.com>
2023-02-09 05:20:05 -0600

    added functions

--- DataTypes/funcs.py
+++ DataTypes/funcs.py
@@ -1,3 +1,5 @@
+
+
 """
 - The difference between arguments and parameters
 - Positional and keyword arguments
@@ -7,3 +9,40 @@
 - Local vs. global arguments
 - Parameter passing (by value or by reference?)
 """
+
+def print_name(name):
+    print(name)
+
+print_name('Alex')
+
+def foo(a, b, c, d=4):
+    print(a, b, c, d)
+
+foo(c=1, b=2, a=3)
+foo(1, b=2, c=3)
+
+foo(1, 2, 3, 7)
+
+def foo2(a, b, *args, **kwargs):
+    print(a, b)
+    for arg in args:
+        print(arg)
+    for key in kwargs:
+        print(key, kwargs[key])
+
+foo2(1, 2, 3, 4, 5, six=6, seven=7)
+
+def foo3(*args, last):
+    for arg in args:
+        print(arg)
+    print(last)
+
+foo3(1, 2, 3, last=100)
+
+def foo5(a, b, c):
+    print(a, b, c)
+
+my_dict= {'a':1, 'b':2, 'c':3}
+foo5(*my_dict) # unpacking arguments
+
+
