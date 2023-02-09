b68374e059df7d944a6cfefab8fd35965b9275e0
nickguerra <nickg@maliciousgames.com>
2023-02-08 18:34:28 -0600

    Added multithreading example

---
+++ DataTypes/funcs.py
@@ -0,0 +1,9 @@
+"""
+- The difference between arguments and parameters
+- Positional and keyword arguments
+- Default arguments
+- Variable-length arguments (*args and **kwargs)
+- Container unpacking into function arguments
+- Local vs. global arguments
+- Parameter passing (by value or by reference?)
+"""
---
+++ DataTypes/multi.py
@@ -0,0 +1,61 @@
+from threading import Thread, Lock, current_thread
+from queue import Queue
+import time
+
+# database_value = 0
+
+# def increase(lock):
+#     global database_value
+
+#     with lock:
+#         local_copy = database_value
+#         # processing
+#         local_copy += 1
+#         time.sleep(0.1)
+#         database_value = local_copy
+
+# if __name__ == "__main__":
+   
+#    lock = Lock()
+#    print('start value', database_value)
+
+#    thread1 = Thread(target=increase, args=(lock,))
+#    thread2 = Thread(target=increase, args=(lock,))
+   
+#    thread1.start()
+#    thread2.start()
+
+#    thread1.join()
+#    thread2.join()
+
+#    print('end value', database_value)
+
+#    print('end main')
+
+def worker(q, lock):
+    while True:
+        value = q.get()
+
+        # processing...
+        with lock:
+            print(f'in {current_thread().name} got {value}')
+            q.task_done()
+            
+
+if __name__ == "__main__":
+    q = Queue()
+    lock = Lock()
+
+    num_threads = 10
+
+    for i in range(num_threads):
+        thread = Thread(target=worker, args=(q, lock))
+        thread.daemon=True # dies when main thread dies, that is why program can end despite worker having an infinite loop
+        thread.start()
+
+    for i in range(1, 21):
+        q.put(i)
+
+    q.join()
+
+    print('end main')
