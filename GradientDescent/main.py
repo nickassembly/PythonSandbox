fce627c1df29c5b442589520367f1c2beacc2b9b
nickguerra <nickg@maliciousgames.com>
2023-05-16 04:45:59 -0500

    started gradient descent problem

--- GradientDescent/main.py
+++ GradientDescent/main.py
@@ -1,1 +1,6 @@
-# placeholder for Gradient Descent example
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def y_function(x):
+    return x ** 2
--- HousingPrices/main.py
+++
@@ -1,1 +0,0 @@
-# placeholder for housing prices exercise
